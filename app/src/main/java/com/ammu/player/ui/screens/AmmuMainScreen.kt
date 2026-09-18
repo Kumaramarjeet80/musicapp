a9c943c567427ddf8b83a7c41830fcfb2ed1aa7b
Kumaramarjeet80 <amarjeetkumar8676840804@gmail.com>
2026-09-19 00:45:26 +0530

    Fix missing Compose UI imports (fillMaxSize, IconButton, clickable)

--- app/src/main/java/com/ammu/player/ui/screens/AmmuMainScreen.kt
+++ app/src/main/java/com/ammu/player/ui/screens/AmmuMainScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.animation.slideInVertically
 import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
